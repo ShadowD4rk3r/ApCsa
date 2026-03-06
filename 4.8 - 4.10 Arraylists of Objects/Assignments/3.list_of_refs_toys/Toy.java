cb147a4daffb2b075f46028f39676774576ea3ba
ShadowD4rk3r <128249140+ShadowD4rk3r@users.noreply.github.com>
2026-03-06 09:15:58 -0500

    Finishing Toy part 1

--- 4.8 - 4.10 Arraylists of Objects/Assignments/3.list_of_refs_toys/Toy.java
+++ 4.8 - 4.10 Arraylists of Objects/Assignments/3.list_of_refs_toys/Toy.java
@@ -5,3 +5,36 @@
 //and sample runner
 //code provided
 //to help you
+
+public class Toy
+{
+    private String name = "";
+    private int count = 0;
+
+    public Toy(String n)
+    {
+        name = n;
+        count = 1;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+
+    public void setCount(int c)
+    {
+        count = c;
+    }
+
+    public String toString()
+    {
+        return name + " " + count;
+    }
+
+}
