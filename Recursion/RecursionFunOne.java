f7f8ecb0380261e93df2b2b1b5d7017ce97bda45
ShadowD4rk3r <128249140+ShadowD4rk3r@users.noreply.github.com>
2026-03-11 12:49:11 +0000

    Made all the classes only FunOne has been modified so far

--- Recursion/RecursionFunOne.java
+++ Recursion/RecursionFunOne.java
@@ -2,6 +2,14 @@ public class RecursionFunOne
 {
     public static int countOddDigits(int num)
     {
-        return 0;
+        if (num == 0)
+        {
+            return 0;
+        }
+        
+        int something = ((num % 10) % 2 == 0 ? 1 : 0);
+
+        return something + countOddDigits(num / 10);
+
     }
 }
