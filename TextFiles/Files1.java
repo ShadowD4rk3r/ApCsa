6cd22ae548cc2ad53bf06d72651c0bcb78ae078f
ShadowD4rk3r <128249140+ShadowD4rk3r@users.noreply.github.com>
2026-04-10 12:37:35 +0000

    Testing things

--- TextFiles/Files1.java
+++ TextFiles/Files1.java
@@ -2,7 +2,37 @@
  *  the total number of numbers, the sum of the numbers, a count
  *  of the even numbers, and the percent of even numbers. * 
  */
+
+import java.io.File;
+import java.util.Scanner;
+
 public class Files1
-{
-    int something = 0;
+{    public static void main(String[] args) throws Exception
+    {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the name of the file to read: ");
+    String fileName = input.nextLine();
+    File myObj = new File(fileName);
+
+    /* Use this if you are unsure if it is working, this is helpful. */
+    Scanner myReader = new Scanner(myObj);
+        try 
+        {
+        
+
+        //System.out.println("File name: " + myObj.nextLine());
+        System.out.println("Absolute path: " + myObj.getAbsolutePath());
+        System.out.println("Writeable: " + myObj.canWrite());
+        System.out.println("Readable " + myObj.canRead());
+        System.out.println("File size in bytes " + myObj.length());
+        } 
+        finally 
+        {
+            myReader.close();
+        } 
+
+
+    }
 }
